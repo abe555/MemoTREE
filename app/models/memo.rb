5afff230a66e19dbc14d03e3b38ec6d6415a076d
abe555 <riho.has3v@gmail.com>
2020-09-12 13:00:50 +0900

    Add memo作成機能

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -5,4 +5,14 @@ class Memo < ApplicationRecord
 
   validates :user_id, presence: true
   validates :body, presence: true
+
+  enum status: {
+  	green: 0,
+  	red: 1,
+  	blue: 2,
+  	yellow: 3,
+  	orange: 4,
+  }
+
+
 end
