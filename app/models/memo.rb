4ff58ce796c4d67a17c3158054743a1767cf32c2
abe555 <riho.has3v@gmail.com>
2020-09-17 23:20:08 +0900

    Add color-change機能

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -11,29 +11,30 @@ class Memo < ApplicationRecord
   	red: 1,
   	blue: 2,
   	yellow: 3,
-  	orange: 4,
+  	gray: 4,
   }
 
   # tag登録機能関係
-  after_create do
-    memo = Memo.find_by(id: self.id)
-    tags = self.body.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
 
-    tags.uniq.map do |t|
-      hashtag = Tag.find_or_create_by(hashname: t.downcase.delete('#'))
-      memo.tags << hashtag
-    end
-  end
+#  after_create do
+#    memo = Memo.find_by(id: self.id)
+#    tags = self.body.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
 
-  before_update do
-    memo = Memo.find_by(id: self.id)
-    tag.tags.clear
-    hashtags = self.body.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |t|
-      hashtag = Tag.find_or_create_by(hashname: tag.downcase.delete('#'))
-      memo.tags << hashtag
-    end
-  end
+#    tags.uniq.map do |t|
+#      hashtag = Tag.find_or_create_by(hashname: t.downcase.delete('#'))
+#      memo.tags << hashtag
+#    end
+#  end
+
+#  before_update do
+#    memo = Memo.find_by(id: self.id)
+#    tag.tags.clear
+#    hashtags = self.body.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+#    hashtags.uniq.map do |t|
+#      hashtag = Tag.find_or_create_by(hashname: tag.downcase.delete('#'))
+#      memo.tags << hashtag
+#    end
+#  end
 
   def self.search(search, word_or_memo_or_tag)
     if word_or_memo_or_tag == "2"
--- app/models/word.rb
+++ app/models/word.rb
@@ -14,7 +14,7 @@ class Word < ApplicationRecord
   	red: 1,
   	blue: 2,
   	yellow: 3,
-  	orange: 4,
+    gray: 4,
   }
 
   def self.search(search, word_or_memo_or_tag)
