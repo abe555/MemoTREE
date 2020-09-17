e8421207c8a28a983f151c96ca379865a1ed6bf2
abe555 <riho.has3v@gmail.com>
2020-09-17 16:15:16 +0900

    Add facebook認証

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
-    # @user = User.find_by(id: :user_id)
   end
 
   def update
     user = current_user
-    # user = User.find_by(id: :user_id)
     if user.update(user_params)
       redirect_to mypage_path, notice: "successfully updated user!"
     else
